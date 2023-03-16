b9bd95c65707470f7b802cfa2171d90ebbc3c921
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-16 18:11:06 +0300

    task1_341

--- task1_34/Program.cs
+++ task1_34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными 
+//положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int [] numbers = new int[size];
+
+void FillArrayRandomNumber(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+FillArrayRandomNumber(numbers);
+Console.WriteLine("Наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] % 2 ==0)
+    count++;
+}
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
